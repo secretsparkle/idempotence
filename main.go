8326bde798b7d4119619c5d3279c40a56d478cce
secretsparkle <scott.madera@gmail.com>
2018-07-10 19:42:56 -0700

    Fixed the board building! Now the buildChessBoard function creates and returns a chess board at starting position.

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 	fmt.Println("map: ", board)
 }
 
+// build the board!
 func buildChessBoard() map[int]string {
 	board := make(map[int]string)
 	for i := 0; i < 8; i++ {
@@ -19,18 +20,21 @@ func buildChessBoard() map[int]string {
 			case 0:
 				switch j {
 				case 0:
-				case 7:
 					board[(i*10)+j] = "wR"
 				case 1:
-				case 6:
 					board[(i*10)+j] = "wKn"
 				case 2:
-				case 5:
 					board[(i*10)+j] = "wB"
 				case 3:
 					board[(i*10)+j] = "wQ"
 				case 4:
 					board[(i*10)+j] = "wK"
+				case 5:
+					board[(i*10)+j] = "wB"
+				case 6:
+					board[(i*10)+j] = "wKn"
+				case 7:
+					board[(i*10)+j] = "wR"
 				}
 			case 1:
 				board[(i*10)+j] = "wP"
@@ -39,18 +43,21 @@ func buildChessBoard() map[int]string {
 			case 7:
 				switch j {
 				case 0:
-				case 7:
 					board[(i*10)+j] = "bR"
 				case 1:
-				case 6:
 					board[(i*10)+j] = "bKn"
 				case 2:
-				case 5:
 					board[(i*10)+j] = "bB"
 				case 3:
 					board[(i*10)+j] = "bQ"
 				case 4:
 					board[(i*10)+j] = "bK"
+				case 5:
+					board[(i*10)+j] = "bB"
+				case 6:
+					board[(i*10)+j] = "bKn"
+				case 7:
+					board[(i*10)+j] = "bR"
 				}
 			default:
 				board[(i*10)+j] = "_" // or this could be nil too
