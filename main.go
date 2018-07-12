227cdd83946cc24509990c8551b67e8e8694ae4b
zep283 <32784449+zep283@users.noreply.github.com>
2018-07-12 16:42:49 -0700

    skeleton for move generation
    also added board printer, because as usual my brain handles more abstract representations poorly

--- main.go
+++ main.go
@@ -8,7 +8,108 @@ import (
 
 func main() {
 	board := buildChessBoard()
-	fmt.Println("map: ", board)
+	printBoard(board)
+}
+
+// prints a more readable board
+func printBoard(board map[int]string) {
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 8; j++ {
+			key := (i * 10) + j
+			fmt.Printf(board[key])
+			if board[key] == "wKn" || board[key] == "bKn" {
+				fmt.Printf("  ")
+			} else if board[key] == "_" {
+				fmt.Printf("    ")
+			} else {
+				fmt.Printf("   ")
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func movePawn(board map[int]string, key int) {
+
+}
+
+func moveRook(board map[int]string, key int) {
+
+}
+
+func moveKnight(board map[int]string, key int) {
+
+}
+
+func moveBishop(board map[int]string, key int) {
+
+}
+
+func moveQueen(board map[int]string, key int) {
+
+}
+
+func moveKing(board map[int]string, key int) {
+
+}
+
+// secondary move generation driver specific to white
+func genWhite(board map[int]string) {
+	pieceCount := 0
+	column := 0
+	row := 0
+	for pieceCount < 16 {
+		key := (row * 10) + column
+		switch board[key] {
+		case "wP":
+			movePawn(board, key)
+		case "wR":
+			moveRook(board, key)
+		case "wKn":
+			moveKnight(board, key)
+		case "wB":
+			moveBishop(board, key)
+		case "wQ":
+			moveQueen(board, key)
+		case "wK":
+			moveKing(board, key)
+		}
+		pieceCount = pieceCount + 1
+	}
+}
+
+// secondary move generation driver specific to black
+func genBlack(board map[int]string) {
+	pieceCount := 0
+	column := 0
+	row := 0
+	for pieceCount < 16 {
+		key := (row * 10) + column
+		switch board[key] {
+		case "bP":
+			movePawn(board, key)
+		case "bR":
+			moveRook(board, key)
+		case "bKn":
+			moveKnight(board, key)
+		case "bB":
+			moveBishop(board, key)
+		case "bQ":
+			moveQueen(board, key)
+		case "bK":
+			moveKing(board, key)
+		}
+		pieceCount = pieceCount + 1
+	}
+}
+
+// driver to produce all available moves from a given board state
+func generateMoves(board map[int]string, player string) {
+	if player == "w" {
+		genWhite(board)
+	} else if player == "b" {
+		genBlack(board)
+	}
 }
 
 // build the board!
